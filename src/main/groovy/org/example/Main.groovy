b31eea0cb8b1805691ee3ea63a3106a0da53a7e5
krainovs <sergejkrainov@mail.ru>
2024-07-31 11:51:01 +0300

    Add first project

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -1,5 +1,11 @@
 package org.example
 
+import static com.google.common.base.Strings.*
 static void main(String[] args) {
+    def str
     println "Hello world!"
+    str = nullToEmpty(str)
+    println "str=" + str
+
+
 }
