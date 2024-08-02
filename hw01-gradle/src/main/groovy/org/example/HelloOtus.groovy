c1662463d806f1fdbc680076b26945ef2970ac7d
krainovs <sergejkrainov@mail.ru>
2024-08-02 09:45:54 +0300

    Add module

---
+++ hw01-gradle/src/main/groovy/org/example/HelloOtus.groovy
@@ -0,0 +1,10 @@
+package org.example
+
+import static com.google.common.base.Strings.*
+
+static void main(String[] args) {
+    def str
+    println "Hello world in module!"
+    str = nullToEmpty(str)
+    println "strInModule=" + str
+}
